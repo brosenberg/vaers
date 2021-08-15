7745e6a4797bd0a972d2d9d1b355833d189b4eed
brosenberg <scdlbx@gmail.com>
2021-08-14 21:53:10 -0600

    Add symptom count and inappropriate age parser

--- process.py
+++ process.py
@@ -3,9 +3,12 @@
 import json
 
 
+SYMPTOMS = ["SYMPTOM1", "SYMPTOM2", "SYMPTOM3", "SYMPTOM4", "SYMPTOM5"]
+
+
 class Vaers(object):
     def __init__(self, dataf, symptomsf, vaxf):
-        # self.data = json.load(open(dataf, encoding='latin1'))
+        self.data = json.load(open(dataf, encoding="latin1"))
         self.symptoms = json.load(open(symptomsf, encoding="latin1"))
         self.vax = json.load(open(vaxf, encoding="latin1"))
 
@@ -19,6 +22,36 @@ class Vaers(object):
             print(f"{k}: {v} ({100*v/total:.2f}%)")
         print()
         print(f"Total: {total}")
+
+    def vax_symptoms(self):
+        vaxes = {}
+        for vid in self.vax:
+            vax = self.vax[vid]["VAX_NAME"]
+            if vax not in vaxes:
+                vaxes[vax] = {}
+            for symptom in SYMPTOMS:
+                try:
+                    if self.symptoms[vid][symptom]:
+                        symp_fix = self.symptoms[vid][symptom].lower()
+                        vaxes[vax][symp_fix] = vaxes[vax].get(symp_fix, 0) + 1
+                except KeyError:
+                    print(f"{vid} is not present in the symptoms DB")
+        for vax in vaxes:
+            print(vax)
+            for k, v in sorted(vaxes[vax].items(), key=lambda x: x[1], reverse=True):
+                print(f"{k}: {v}")
+            print()
+
+    def inapp_age(self):
+        for vid in self.symptoms:
+            for symptom in SYMPTOMS:
+                if "inappropriate age" in self.symptoms[vid][symptom]:
+                    try:
+                        text = self.data[vid]["SYMPTOM_TEXT"]
+                        age = self.data[vid]["AGE_YRS"]
+                    except KeyError:
+                        print(f"{vid} not in Data DB")
+                    print(f"{vid} (age:{age}): {text}")
 
 
 def find_keys_ret_id(src, keys, match):
@@ -71,15 +104,22 @@ Total 167699170
     )
 
 
-def main():
-    vaers = Vaers("2021VAERSDATA.json", "2021VAERSSYMPTOMS.json", "2021VAERSVAX.json")
-    # count_key(vaers.vax, "VAX_NAME", match="COVID")
+def find_strokes(vaers):
     stroke_ids = find_keys_ret_id(
         vaers.symptoms,
-        ["SYMPTOM1", "SYMPTOM2", "SYMPTOM3", "SYMPTOM4", "SYMPTOM5"],
+        SYMPTOMS,
         "stroke",
     )
     vaers.print_vaccine_types(stroke_ids)
+
+
+def vaccine_counts(vaers):
+    count_key(vaers.vax, "VAX_NAME", match="COVID")
+
+
+def main():
+    vaers = Vaers("2021VAERSDATA.json", "2021VAERSSYMPTOMS.json", "2021VAERSVAX.json")
+    vaers.inapp_age()
 
 
 if __name__ == "__main__":
