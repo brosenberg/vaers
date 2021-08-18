91df802a6a77ae91e60411059b8547f233d3240b
brosenberg <scdlbx@gmail.com>
2021-08-18 10:36:46 -0600

    Improve converter slightly

--- csv/convert.py
+++ csv/convert.py
@@ -3,18 +3,20 @@
 import csv
 import json
 import pandas
+import sys
 
 
-def csv_to_json(base_path):
+def make_json_pandas(base_path):
     csv_file = f"{base_path}.csv"
     json_file = f"{base_path}.json"
     data = pandas.read_csv(csv_file, encoding="latin1", low_memory=False)
     data.to_json(json_file)
 
 
-def make_json(base_path, key="VAERS_ID"):
-    csv_file_path = f"{base_path}.csv"
-    json_file_path = f"{base_path}.json"
+def make_json(csv_file_path, key="VAERS_ID"):
+    json_file_path = csv_file_path.replace(".csv", ".json")
+    if not json_file_path.endswith(".json"):
+        json_file_path += ".json"
     data = {}
 
     with open(csv_file_path, encoding="latin1") as csvf:
@@ -26,10 +28,18 @@ def make_json(base_path, key="VAERS_ID"):
         jsonf.write(json.dumps(data, indent=4))
 
 
+# def vaers_dump(data, symptoms, vax):
+#    def get_csv_handle(fh):
+#        return csv.DictReader(open(fname, encoding='latin1'))
+#    data = {}
+#    pk = 'VAERS_ID'
+#    with get_csv_handle(data) as datafh:
+#
+
+
 def main():
-    make_json("2019VAERSDATA")
-    make_json("2019VAERSSYMPTOMS")
-    make_json("2019VAERSVAX")
+    for fname in sys.argv[1:]:
+        make_json(fname)
 
 
 if __name__ == "__main__":
